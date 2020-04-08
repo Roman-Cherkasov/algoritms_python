d2b240f4e9d2e023f342b16affc3aa6360cf95af
romen <rcherkasov@ukr.net>
2020-04-09 01:27:50 +0300

    sentinel_linar_search

--- linear_search.py
+++ linear_search.py
@@ -12,3 +12,20 @@ def better_linar_search(A, n, x):
             return i
     else:
         return "NOT-FOUND"
+
+def sentinel_linar_search(A, n, x):
+    last = A[n - 1]
+    A[n - 1] = x
+    i = 0
+    while (A[i] != x):
+        i += 1
+    A[n - 1] = last
+    if ((i < n - 1) or (A[n - 1] == x)):
+        answer = i
+    else:
+        answer = "NOT-FOUND"
+    return answer
+
+a = [5, 15, 9, 22, 4, 9, 66, 16600, 100, 10]
+
+print(sentinel_linar_search(a, len(a), 66))
